a80344284b00b0d6f03c53a18b7e9ff57b929331
jkoehn90 <51415504+jkoehn90@users.noreply.github.com>
2026-04-30 13:06:42 -0700

    add SecurityConfig class and dependency changes

---
+++ src/main/java/com/jkoehn90/apigateway/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.jkoehn90.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll()   // Gateway's AuthenticationFilter handles auth
+                );
+
+        return http.build();
+    }
+}
